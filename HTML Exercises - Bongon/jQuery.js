457af18a466b84d221d878c133b6bee1c5a5e8c7
FrancesBongon <fneb2023-1205-64848@bicol-u.edu.ph>
2025-09-26 16:48:24 +0800

    Update jQuery.js

--- HTML Exercises - Bongon/jQuery.js
+++ HTML Exercises - Bongon/jQuery.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function(){
+    $("#demo").text("Hello jQuery!");
+    $("#box").css("background-color", "lightblue");
+    $("#btn").click(function(){
+    $("#demo").hide();
+    });
+    $("#btn").click(function(){
+    $("#demo").show();
+    });
+    $("#btn").click(function(){
+    $("#demo").toggle();
+    });
+    $("#demo").addClass("highlight");
+});
